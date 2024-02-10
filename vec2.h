a8e2c07882a6d878c52fa340acba86f5fa3f451f
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2024-02-10 01:29:49 -0500

    Update vec2.h
    Added length function.

--- vec2.h
+++ vec2.h
@@ -1,5 +1,12 @@
+#include <cmath>
+
 template <typename T>
 struct vec2<T> {
     T x;
     T y;
 };
+
+template <typename T>
+T length(const vec2<T>& vec) {
+    return sqrt(vec.x * vec.x + vec.y * vec.y);
+}

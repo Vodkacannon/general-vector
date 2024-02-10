cd369afc1d3d18c0556b640518b03b04892cbb63
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2024-02-10 01:38:06 -0500

    Update vec2.h

--- vec2.h
+++ vec2.h
@@ -10,3 +10,9 @@ template <typename T>
 T length(const vec2<T>& vec) {
     return sqrt(vec.x * vec.x + vec.y * vec.y);
 }
+
+template <typename T>
+bool operator==(vec2<T> lhs, vec2<T> rhs) {
+    if (lhs.x == rhs.x && lhs.y == rhs.y) return true;
+    return false;
+}

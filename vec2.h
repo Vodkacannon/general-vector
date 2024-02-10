ea7304a67b93f00b85e389d58423dcd12360f882
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2024-02-10 01:43:09 -0500

    Update vec2.h
    Added equality op and equals op.

--- vec2.h
+++ vec2.h
@@ -7,12 +7,20 @@ struct vec2<T> {
 };
 
 template <typename T>
-T length(const vec2<T>& vec) {
-    return sqrt(vec.x * vec.x + vec.y * vec.y);
+vec2<T> operator=(vec2<T>& rhs) {
+    return vec2<T> { rhs.x, rhs.y };
 }
+    
 
 template <typename T>
 bool operator==(vec2<T> lhs, vec2<T> rhs) {
     if (lhs.x == rhs.x && lhs.y == rhs.y) return true;
     return false;
 }
+
+template <typename T>
+T length(const vec2<T>& vec) {
+    return sqrt(vec.x * vec.x + vec.y * vec.y);
+}
+
+
